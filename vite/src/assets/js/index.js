1a8c355444a52515c613f1eba9fd52dc6a3de0f2
wbq3510 <baran.dariusz@hotmail.com>
2023-08-14 20:46:19 +0200

    search icon

--- vite/src/assets/js/index.js
+++ vite/src/assets/js/index.js
@@ -1,2 +1,8 @@
 import '../css/index.css'
 import './navigation'
+
+const searchIcon = document.querySelector('#search-icon')
+const searchInput = document.querySelector('#search-input')
+searchIcon.addEventListener('click', function (e) {
+	searchInput.classList.toggle('hidden')
+})
